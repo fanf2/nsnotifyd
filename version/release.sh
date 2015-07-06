a950d1208f1c116ec8f1185e45607d3d946476f7
Tony Finch <dot@dotat.at>
2015-07-06 16:39:34 +0100

    version: better way to ensure version.h is in release tarballs

--- version/release.sh
+++ version/release.sh
@@ -6,7 +6,7 @@ eval $(sed 's/#define //;s/ /=/' version.h)
 V=$VERSION
 
 mkdir $V
-for f in $(git ls-files | fgrep -v .git) "$@"
+for f in $(git ls-files | fgrep -v .git) version.h "$@"
 do	d=$(dirname $f)
 	mkdir -p $V/$d
 	cp $f $V/$f
