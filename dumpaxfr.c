b478a57731060805904b7e145278648aa6312daf
Tony Finch <dot@dotat.at>
2026-02-24 17:58:42 +0000

    dumpaxfr: add licence header

--- dumpaxfr.c
+++ dumpaxfr.c
@@ -1,3 +1,17 @@
+/* dumpaxfr: capture a DNS zone transfer
+ *
+ * Written by Tony Finch <dot@dotat.at> in Cambridge.
+ *
+ * Permission is hereby granted to use, copy, modify, and/or
+ * distribute this software for any purpose with or without fee.
+ *
+ * This software is provided 'as is', without warranty of any kind.
+ * In no event shall the authors be liable for any damages arising
+ * from the use of this software.
+ *
+ * SPDX-License-Identifier: 0BSD OR MIT-0
+ */
+
 #define BIND_8_COMPAT
 
 #include <arpa/nameser.h>
